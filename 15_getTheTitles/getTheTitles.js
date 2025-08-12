27e4e4a1d70d19e36400533021a5832c78afa9d3
Kallam <kallamsamad@gmail.com>
2025-08-12 19:17:22 +0100

    got the titles by mapping the list

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,27 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Limitless',
+    author: 'Name'
+  },
+  {
+    title: 'The lord of the flies',
+    author: 'Name2'
+  },
+  {
+      title: "Catcher in the Rye",
+      author: "JD Sellinger"
+  },
+  {
+      title: "Dont",
+      author: "do it"
+  }
+]
 
-};
+const bookList = books.map((a)=>{
+    return a.title
+})
+
+console.log(bookList)
 
 // Do not edit below this line
 module.exports = getTheTitles;
